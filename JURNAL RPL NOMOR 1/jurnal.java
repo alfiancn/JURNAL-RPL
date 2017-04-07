228acd670ad1556f2a98f12b88b29dfb5899e9a4
alfiancn_6706154146 <snila713@gmail.com>
2017-04-07 10:10:41 +0700

    Update jurnal.java

--- JURNAL RPL NOMOR 1/jurnal.java
+++ JURNAL RPL NOMOR 1/jurnal.java
@@ -1,19 +1,21 @@
-import java.util.scanner;
 
-public class jurnal1{
+package javaapplication1;
+import java.util.Scanner;
+
+public class JavaApplication1{
 	public static void main(String[]args){
 		Scanner scn = new Scanner(System.in);
 		System.out.print("Masukkan Angka : ");
-		int x = show.nextInt();
+		int x = scn.nextInt();
 		long fibo[] = new long[x];
-		fibo[0]=0;
-		fibo[1]=1;
+		fibo[0]=1;
+		fibo[1]=2;
 		
 		for (int i=2 ; i<x; i++){
 			fibo[i] = fibo[i-1] + fibo[i-2];
 		}
 		for (int i=0 ; i<x; i++){
-			System.out.print(fib[i]+" ");
+			System.out.print(fibo[i]+" ");
 		}
 	}
 }
